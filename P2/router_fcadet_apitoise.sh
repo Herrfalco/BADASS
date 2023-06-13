03c1e62ef6e4d44c9ffad6abc9741b602d35c716
Florian Cadet <cadet.florian@gmail.com>
2023-06-13 18:24:35 +0200

    add multicast

--- P2/router_fcadet_apitoise.sh
+++ P2/router_fcadet_apitoise.sh
@@ -1,16 +1,18 @@
 THIS=$(echo $HOSTNAME | tr -dc '0-9')
 OTHER=$(if [ $THIS = "1" ]; then echo 2; else echo 1; fi;)
+VXLAN_CONF=$(if [$1 = "multi"]
+	then echo group 239.1.1.1;
+	else echo remote 10.1.1.$OTHER local 10.1.1.$THIS; fi)
 
 #setup ip address 10.1.1.X on eth0
 ip addr add 10.1.1.$THIS/24 dev eth0
 
 #create vxlan between 10.1.1.X
-ip link add name vxlan10 \
+ip link add name vxlan10
 	type vxlan \
 	id 10 \
 	dev eth0 \
-	remote 10.1.1.$OTHER \
-	local 10.1.1.$THIS \
+	$VXLAN_CONF \
 	dstport 4789
 ip link set dev vxlan10 up
 
