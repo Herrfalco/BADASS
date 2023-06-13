0bcd41dde3f9f8c7a63b1808b25dd4865377b600
Florian Cadet <cadet.florian@gmail.com>
2023-06-13 16:27:38 +0200

    test

--- P2/router_fcadet_apitoise.sh
+++ P2/router_fcadet_apitoise.sh
@@ -1,10 +1,13 @@
 THIS=$(echo $HOSTNAME | tr -dc '0-9')
 OTHER=$(if [ $THIS = "1" ]; then echo 2; else echo 1; fi;)
 
-#setup ip address 10... of interface connected to host
+#start routing daemons
+/usr/lib/frr/frrinit.sh start
+
+#setup ip address 10.1.1.X on eth0 (connected to host)
 ip addr add 10.1.1.$THIS/24 dev eth0
 
-#create vxlan with id 10 through ip address 20...
+#create vxlan with ip address 20.1.1.X
 ip link add name vxlan10 \
 	type vxlan \
 	id 10 \
@@ -20,8 +23,5 @@ ip link add br0 type bridge
 ip link set dev br0 up
 brctl addif br0 eth1
 brctl addif br0 vxlan10
-
-#start routing daemons
-/usr/lib/frr/frrinit.sh start
 
 tail -f /dev/null
