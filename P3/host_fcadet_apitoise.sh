e4595829ce223507541f179353234c6ae2dd7e7d
Florian Cadet <cadet.florian@gmail.com>
2023-06-14 18:39:48 +0200

    30 to 20

--- P3/host_fcadet_apitoise.sh
+++ P3/host_fcadet_apitoise.sh
@@ -2,6 +2,6 @@ HOSTNB=$(echo $HOSTNAME | tr -dc '0-9')
 DEVNB=$(if [ "$HOSTNB" = "1" ]; then echo 1; else echo 0; fi;)
 
 #setup ip address
-ip addr add 30.1.1.$HOSTNB/24 dev eth$DEVNB
+ip addr add 20.1.1.$HOSTNB/24 dev eth$DEVNB
 
 tail -f /dev/null
