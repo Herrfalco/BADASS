8504fa3cf53d4420bb2effbdd4796b8778360c58
Florian Cadet <cadet.florian@gmail.com>
2023-06-14 19:04:09 +0200

    add vxlan script for P3

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,7 @@ FROM frrouting/frr AS router
 ADD https://raw.githubusercontent.com/Herrfalco/BADASS/main/Utils/daemons.conf /etc/frr/daemons
 ADD https://raw.githubusercontent.com/Herrfalco/BADASS/main/P1/router_fcadet_apitoise.sh /P1.sh
 ADD https://raw.githubusercontent.com/Herrfalco/BADASS/main/P2/router_fcadet_apitoise.sh /P2.sh
+ADD https://raw.githubusercontent.com/Herrfalco/BADASS/main/P3/router_fcadet_apitoise.sh /P3.sh
 
 #############################################################################################
 
--- P3/router_fcadet_apitoise.sh
+++ P3/router_fcadet_apitoise.sh
@@ -1,25 +1,14 @@
-THIS=$(echo $HOSTNAME | tr -dc '0-9')
-OTHER=$(if [ "$THIS" = "1" ]; then echo 2; else echo 1; fi;)
-VXLAN_CONF=$(if [ "$1" = "multi" ]
-	then echo group 239.1.1.1;
-	else echo remote 10.1.1.$OTHER local 10.1.1.$THIS; fi)
+ROUTNB=$(echo $HOSTNAME | tr -dc '0-9')
+DEVNB=$(if [ "$ROUTNB" = "2" ]; then echo 1; else echo 0; fi;)
 
-#setup ip address 10.1.1.X on eth0
-ip addr add 10.1.1.$THIS/24 dev eth0
-
-#create vxlan between 10.1.1.X
-ip link add name vxlan10 \
-	type vxlan \
-	id 10 \
-	dev eth0 \
-	$VXLAN_CONF \
-	dstport 4789
+#create vxlan
+ip link add name vxlan10 type vxlan id 10 dstport 4789
 ip link set dev vxlan10 up
 
-#bridge vxlan to eth1 interface (connected to host)
+#bridge vxlan to VTEP interface
 ip link add br0 type bridge
 ip link set dev br0 up
-brctl addif br0 eth1
+brctl addif br0 eth$DEVNB
 brctl addif br0 vxlan10
 
 #start routing daemons
