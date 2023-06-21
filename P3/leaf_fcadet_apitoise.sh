9090cc0440ef73d5ccbcb77466dde5ae308354c7
Herrfalco <cadet.florian@gmail.com>
2023-06-21 13:28:13 +0200

    add bgp + comments

--- P3/leaf_fcadet_apitoise.sh
+++ P3/leaf_fcadet_apitoise.sh
@@ -4,7 +4,7 @@ LEAFNB=$(echo $HOSTNAME | tr -dc '0-9')
 ip link add name vxlan10 type vxlan id 10 dstport 4789
 ip link set dev vxlan10 up
 
-#bridge vxlan to VTEP interface
+#bridge vxlan to eth0
 ip link add br0 type bridge
 ip link set dev br0 up
 brctl addif br0 eth0
@@ -17,17 +17,51 @@ frr defaults traditional
 hostname $HOSTNAME
 no ipv6 forwarding
 !
+######################
+!
+# INTERFACE TO SPINE
+# (populated with OSPF in area 0)
+!
 interface eth1
-ip address 10.1.$LEAFNB.2/24
-ip ospf area 0
+ ip address 10.1.$LEAFNB.2/24
+ ip ospf area 0
 exit
+!
+######################
+!
+# LOOPBACK ADDRESS (lo)
+# (populated with ospf in area 0)
 !
 interface lo
-ip address $LEAFNB.$LEAFNB.$LEAFNB.$LEAFNB/32
-ip ospf area 0
+ ip address 1.1.1.$LEAFNB/32
+ ip ospf area 0
 exit
 !
+######################
+!
+# OSPF CONFIGRATION
+!
 router ospf
+exit
+!
+######################
+!
+# BGP CONFIGURATION
+# (AS 1 to 1 -> iBGP)
+!
+router bgp 1
+ !
+ # (Route Reflector configuration)
+ !
+ neighbor 1.1.1.4 remote-as 1
+ neighbor 1.1.1.4 update-source lo
+ !
+ # (EVPN configuration for MAC and VNI support)
+ !
+ address-family l2vpn evpn
+  neighbor 1.1.1.4 activate
+  advertise-all-vni
+ exit-address-family
 exit
 !
 end" > /etc/frr/frr.conf
--- P3/spine_fcadet_apitoise.sh
+++ P3/spine_fcadet_apitoise.sh
@@ -2,28 +2,63 @@ echo "!
 !
 frr version 8.4_git
 frr defaults traditional
-hostname router_fcadet_apitoise-$LEAFNB
+hostname $HOSTNAME
 no ipv6 forwarding
 !
 !
+######################
+!
+# INTERFACES TO LEAFS
+!
 interface eth0
-ip address 10.1.1.1/24
+ ip address 10.1.1.1/24
 exit
 !
 interface eth1
-ip address 10.1.2.1/24
+ ip address 10.1.2.1/24
 exit
 !
 interface eth2
-ip address 10.1.3.1/24
+ ip address 10.1.3.1/24
 exit
+!
+######################
+!
+# LOOPBACK ADDRESS
 !
 interface lo
-ip address 4.4.4.4/32
+ ip address 1.1.1.4/32
 exit
 !
+######################
+!
+# OSPF CONFIGRATION
+# (spread populated adresses in area 0)
+!
 router ospf
-network 10.1.0.0/16 area 0
+ network 0.0.0.0/0 area 0
+exit
+!
+######################
+!
+# BGP CONFIGURATION
+# (AS 1 to 1 -> iBGP)
+!
+router bgp 1
+ !
+ # (Peer group 'leafs' configuration)
+ !
+ neighbor leafs peer-group
+ neighbor leafs remote-as 1
+ neighbor leafs update-source lo
+ bgp listen range 1.1.1.0/24 peer-group leafs
+ !
+ # (EVPN configuration for MAC and VNI support)
+ !
+ address-family l2vpn evpn
+  neighbor leafs activate
+  neighbor leafs route-reflector-client
+ exit-address-family
 exit
 !
 end" > /etc/frr/frr.conf
